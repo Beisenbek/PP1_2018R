ffeff8305038f42f1d14947c427b543fc174598b
bsnbk <beysenbek@gmail.com>
2018-01-31 19:12:27 +0600

    'upd'

--- W4/G2/D1/5_1.cpp
+++ W4/G2/D1/5_1.cpp
@@ -11,10 +11,15 @@ int main(){
 	int x;
 	cin >> x;
 
-	do{
+	cout << x % 10 << endl;
+	x = x / 10;
+
+	while(x > 0){
 		cout << x % 10 << endl;
 		x = x / 10;
-	}while(x > 0);
+	}
+
+
 	
 	return 0;
 }
