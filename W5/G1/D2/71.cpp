bf0c9bb202db81c00122940a23d1c3543af12afa
bsnbk <beysenbek@gmail.com>
2018-02-10 11:28:58 +0600

    'upd'

--- W5/G1/D2/71.cpp
+++ W5/G1/D2/71.cpp
@@ -10,6 +10,30 @@ int main(){
 
 	freopen("input.txt","r",stdin);
 
+	int n;
+	cin >> n;
+	int a[n];
+
+	for(int i = 0; i < n; ++i){
+		cin >> a[i];
+	}
+
+	int b[n];
+
+
+	/*b[1] = a[0];
+	b[2] = a[1];
+	...
+	b[n-1] = a[n-2];*/
+
+	for(int i = 1; i < n; ++i){
+		b[i] = a[i-1];
+	}
+	b[0] = a[n-1];
+
+	for(int i = 0; i < n; ++i){
+		cout << b[i] << " ";
+	}
 	
 	return 0;
 }
