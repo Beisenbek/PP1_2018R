e7e367f8659f477f5b0cd6dc2283cdbf4f72397c
Beisenbek Baisakov <beysenbek@gmail.com>
2018-02-24 11:39:35 +0600

    upd

--- W7/G1/D2/3124.cpp
+++ W7/G1/D2/3124.cpp
@@ -7,8 +7,24 @@ int main(){
     string str;
     getline(cin,str);
 
-    cout << str.find_first_of('h') << endl;
-    cout << str.find_last_of('h') << endl;
+    int l = str.find_first_of('h');
+    int r = str.find_last_of('h');
+    
+    for(int i = 0; i <=l; ++i){
+        cout << str[i];
+    }
+
+    for(int i = 0; i < 2; ++i){
+        for(int j = l + 1; j< r; ++j){
+            cout << str[j];
+        }
+    }
+
+    for(int i = r; i < str.size(); ++i){
+        cout << str[i];
+    }
+
+    cout << endl;
 
     return 0;
 }
